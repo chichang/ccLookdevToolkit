2e75763c9f6ea35a5eee81417cb2d165e036880b
ChiChang Chu <chichang@mrxfx.com>
2015-01-09 10:37:43 -0500

    add getTexStatus.py

---
+++ getTexStatus.py
@@ -0,0 +1,50 @@
+import maya.cmds as mc
+import os
+
+def texStatus():
+    #quick check stats for all the texture files in the scene.
+
+    dirs = []
+    fileList = mc.ls(type = "file")
+    
+    #prints out all files and their absolute path.
+    print "\n========= All File Path ==============="
+    print "Found", str(len(fileList)), "files in the scene:\n"
+    
+    for f in fileList:
+        fPath = mc.getAttr(f+".fileTextureName")
+        
+        dirPath = os.path.split(fPath)[0]
+        
+        print f + ":"
+        print fPath + "\n"
+
+        if dirPath not in dirs:
+            dirs.append(dirPath)
+
+    #prints out all folders being used for the textures
+    print "\n========= All FOLDERS USED ==============="
+    for p in dirs:
+        print p
+
+    #prints out the folder is doesnt's exist
+    print "\n========= File Path not Exist ==============="
+    for p in dirs:
+        try:
+            if not os.path.exists(p):
+                print p
+        except e:
+            print error
+
+    #prints out the folder if it's empty
+    print "\n========= File Path Empty ==============="
+    
+    for p in dirs:
+        try:
+            if len(os.listdir(p)) == 0:
+                print p
+        except:
+            pass
+
+#run
+texStatus()
